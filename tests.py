d4b1da9c869f2037175b43c9f0c72eb858e4e79e
bthee <b.theeck08@gmail.com>
2024-01-15 17:31:59 +0100

    added break test

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main import Maze
+from main import Maze, Window
 
 class Tests(unittest.TestCase):
     def test_maze_create_cells(self):
@@ -15,6 +15,13 @@ class Tests(unittest.TestCase):
         self.assertEqual(len(m1._cells), 1)
         self.assertEqual(len(m1._cells[0]), 1)
         
+    def test_break_entrance_and_exit(self):
+        win = Window(900, 700)
+        maze = Maze(0, 0, 12, 16, 50, 50, win)
+        maze._break_entrance_and_exit()
+        assert not maze._cells[0][0].has_top_wall, "Entrance wall is not broken"
+        assert not maze._cells[-1][-1].has_bottom_wall, "Exit wall is not broken"
+
 
 if __name__ == "__main__":
     unittest.main()
