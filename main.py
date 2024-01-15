ff470d5cad7adce0b454970e5ad7fd4c9bfa9198
bthee <b.theeck08@gmail.com>
2024-01-15 15:17:32 +0100

    updated Cell class

--- main.py
+++ main.py
@@ -43,11 +43,50 @@ class Line:
         canvas.create_line(self.point_a.x, self.point_a.y, self.point_b.x, self.point_b.y, fill=fill_color, width=2)
 
 
+class Cell:
+    def __init__(self, has_left_wall, has_right_wall, has_top_wall, has_bottom_wall, _x1, _y1, _x2, _y2, _win):
+        self.has_left_wall = has_left_wall
+        self.has_right_wall = has_right_wall
+        self.has_top_wall = has_top_wall
+        self.has_bottom_wall = has_bottom_wall
+        self._x1 = _x1
+        self._x2 = _x2
+        self._y1 = _y1
+        self._y2 = _y2
+        self._win = _win
+
+    def draw(self):
+        if self.has_left_wall:
+            left_wall = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            self._win.draw_line(left_wall, "black")
+        if self.has_right_wall:
+            right_wall = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            self._win.draw_line(right_wall, "black")
+        if self.has_top_wall:
+            top_wall = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+            self._win.draw_line(top_wall, "black")
+        if self.has_bottom_wall:
+            bottom_wall = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+            self._win.draw_line(bottom_wall, "black")
+
+    def draw_move(self, to_cell, undo=False):
+        color = "gray" if undo else "red"
+
+        x1 = (self._x1 + self._x2) / 2
+        y1 = (self._y1 + self._y2) / 2
+
+        x2 = (to_cell._x1 + to_cell._x2) / 2
+        y2 = (to_cell._y1 + to_cell._y2) / 2
+
+        move_line = Line(Point(x1, y1), Point(x2, y2))
+        self._win.draw_line(move_line, color)
+
 
 if __name__ == "__main__":
     win = Window(800, 600)
-    p1 = Point(100, 100)
-    p2 = Point(100, 200)
-    line = Line(p1, p2)
-    win.draw_line(line, "red")
+    c1 = Cell(True, True, True, True, 0, 0, 100, 100, win)
+    c1.draw()
+    c2 = Cell(True, True, True, True, 100, 0, 200, 100, win)
+    c2.draw()
+    c1.draw_move(c2)
     win.wait_for_close()
